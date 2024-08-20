b6270b37a7216e4a1c9c029ba81373d4d5d60c42
vinigalvaobombs <154466936+vinigalvaobombs@users.noreply.github.com>
2024-08-20 00:23:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
@@ -35,7 +35,7 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 3 + 1);
+    return parseInt(Math.random() * 100 + 1);
 }
 
 function limparCampo() {
